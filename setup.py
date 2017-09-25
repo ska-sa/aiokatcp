8e318e1469de65d105a0565ed00b579d5e417de2
Bruce Merry <bmerry@gmail.com>
2017-09-25 08:26:12 +0200

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+tests_require = ['asynctest']
+docs_require = ['sphinx', 'sphinx-autodoc-typehints', 'sphinxcontrib-asyncio']
+
+setup(
+    name='aiokatcp',
+    version='0.1',
+    packages=find_packages(),
+    author='Bruce Merry',
+    author_email='bmerry@ska.ac.za',
+    description='Asynchronous I/O implementation of the katcp protocol',
+    keywords='asyncio katcp',
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+        'doc': docs_require
+    },
+    python_requires='>=3.5'
+)
