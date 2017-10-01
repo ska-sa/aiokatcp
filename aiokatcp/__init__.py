0264401ede68e109cad9e0db6e9eae99dba726c5
Bruce Merry <bmerry@gmail.com>
2017-10-01 21:44:33 +0200

    Import SensorSet in top-level namespace

--- aiokatcp/__init__.py
+++ aiokatcp/__init__.py
@@ -2,5 +2,5 @@ from .core import (                                   # noqa: F401
     Message, KatcpSyntaxError, Address, Timestamp,
     encode, decode, register_type, get_type, TypeInfo)
 from .connection import Connection, FailReply         # noqa: F401
-from .server import DeviceServer, RequestContext      # noqa: F401
+from .server import DeviceServer, RequestContext, SensorSet      # noqa: F401
 from .sensor import Reading, Sensor, SensorSampler    # noqa: F401
