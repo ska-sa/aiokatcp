cd924e94ca7f0b01fa772d6814b88aa59534ac6c
Bruce Merry <bmerry@gmail.com>
2017-11-11 18:16:27 +0200

    Flake8 fix

--- aiokatcp/__init__.py
+++ aiokatcp/__init__.py
@@ -36,12 +36,13 @@ else:
     __version__ = _katversion.get_version(__path__[0])   # type: ignore  # mypy issue 1422
 # END VERSION CHECK
 
-def minor_version():
-    return '.'.join(__version__.split('.')[:2])
-
 from .core import (                                   # noqa: F401
     Message, KatcpSyntaxError, Address, Timestamp,
     encode, decode, register_type, get_type, TypeInfo)
 from .connection import Connection, FailReply         # noqa: F401
 from .server import DeviceServer, RequestContext, SensorSet      # noqa: F401
 from .sensor import Reading, Sensor, SensorSampler    # noqa: F401
+
+
+def minor_version():
+    return '.'.join(__version__.split('.')[:2])
