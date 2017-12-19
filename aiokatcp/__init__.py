f0420f2e0f9d366352f5dd23f9a6a01d31ff5b28
Bruce Merry <bmerry@ska.ac.za>
2017-12-19 16:06:04 +0200

    Import ProtocolError into top-level namespace

--- aiokatcp/__init__.py
+++ aiokatcp/__init__.py
@@ -41,7 +41,7 @@ from .core import (                                   # noqa: F401
     encode, decode, register_type, get_type, TypeInfo)
 from .connection import Connection, FailReply, InvalidReply      # noqa: F401
 from .server import DeviceServer, RequestContext, SensorSet      # noqa: F401
-from .client import Client                            # noqa: F401
+from .client import Client, ProtocolError             # noqa: F401
 from .sensor import Reading, Sensor, SensorSampler    # noqa: F401
 
 
