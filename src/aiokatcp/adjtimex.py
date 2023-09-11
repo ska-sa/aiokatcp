5f608a30b5059b59f53e18857dbf005422e31105
Ludwig Schwardt <ludwig@ska.ac.za>
2023-09-11 17:56:04 +0200

    Bump the year in the copyright notice

--- src/aiokatcp/adjtimex.py
+++ src/aiokatcp/adjtimex.py
@@ -1,4 +1,4 @@
-# Copyright 2022 National Research Foundation (SARAO)
+# Copyright 2022, 2023 National Research Foundation (SARAO)
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
